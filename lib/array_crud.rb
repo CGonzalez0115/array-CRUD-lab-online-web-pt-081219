ccc8ee50966a4802fecc59700b489a17202d9f6b
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 04:47:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  words = ["you", "am", "car", "shoes"]
+  words = ["you", "car", "am", "shoes"]
 
   words[2]
 
