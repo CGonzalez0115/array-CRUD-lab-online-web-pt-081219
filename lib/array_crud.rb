190e2ff077dfcbaa0f713c420d04f6619d72fe8f
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 04:26:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- big_cats.push("snow leopard")
+  big_cats = ["cheetah", "lion", "Bengal tiger", "leopard"]
+  big_cats.push("snow leopard")
 
 end
 
