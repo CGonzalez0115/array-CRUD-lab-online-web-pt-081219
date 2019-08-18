bf91d1c8cad6965c7f356ddc09dbdf13537754f3
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 04:43:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["red", "blue", "indigo", "wow"]
+  array = ["wow", "blue", "indigo", "orange"]
 
   wow_array = array.shift
 
